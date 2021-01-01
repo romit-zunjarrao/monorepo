0ffce44f6ff672abdb27f340762318e154bc8438
Romit <romitzunjarrao71@gmail.com>
2021-01-01 19:04:42 +0530

    Added basic functionality for display

--- android/CovidTracker/app/src/main/java/com/project/covidtracker/ui/CountryFragment.java
+++ android/CovidTracker/app/src/main/java/com/project/covidtracker/ui/CountryFragment.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.anychart.APIlib;
 import com.anychart.AnyChart;
@@ -39,6 +41,8 @@ public class CountryFragment extends Fragment {
     private Pie line;
     private static final String TAG = "CountryFragment";
     AnyChartView countryPieView, countryLineView;
+    EditText countryEditText;
+    Button searchButton;
 
     public CountryFragment() {
         // Required empty public constructor
@@ -61,30 +65,32 @@ public class CountryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         countryPieView = (AnyChartView) view.findViewById(R.id.country_pie_chart);
         countryLineView = (AnyChartView) view.findViewById(R.id.country_line_chart);
-        getCountryLatestData();
-
-        new Handler(Looper.getMainLooper()).postDelayed(()-> {
-                    getCountryDailyData();
-                },2000
-            );
+        countryEditText = view.findViewById(R.id.country_edit_text);
+        searchButton = view.findViewById(R.id.search_button);
+        searchButton.setOnClickListener((View v)->{
+            if(countryEditText.length() != 0){
+                searchCountry(countryEditText.getText().toString().trim());
+            }
+        });
+        searchCountry("Italy");
     }
 
 
-    private void getCountryLatestData() {
+    private void getCountryLatestData(String countryName) {
         Retrofit retrofit = RetrofitClientInstance.getRetrofitInstance(getActivity().getApplicationContext());
         CovidDataApi covidDataApi = retrofit.create(CovidDataApi.class);
-        Call<CountryLatest[]> call = covidDataApi.getLatestCountryData("Italy");
+        Call<CountryLatest[]> call = covidDataApi.getLatestCountryData(countryName);
         call.enqueue(new Callback<CountryLatest[]>() {
             @Override
             public void onResponse(Call<CountryLatest[]> call, Response<CountryLatest[]> response) {
                 CountryLatest[] countryLatest = response.body();
                 List<DataEntry> data = new ArrayList<>();
                 Log.d(TAG, "onResponse: CountryLatest response" + response.body());
-                Log.d(TAG, "onResponse: CountryLatest response" + countryLatest.length);
                 data.add(new ValueDataEntry("confirmed", countryLatest[0].getConfirmed()));
                 data.add(new ValueDataEntry("recovered", countryLatest[0].getRecovered()));
                 data.add(new ValueDataEntry("deaths", countryLatest[0].getDeaths()));
                 data.add(new ValueDataEntry("critical", countryLatest[0].getCritical()));
+                Log.d(TAG, "onResponse:  CountryName returned by API" +countryLatest[0].getCountry() + "\n"+  countryLatest[0].getConfirmed());
                 setCountryPieChart(data);
             }
 
@@ -97,22 +103,22 @@ public class CountryFragment extends Fragment {
         });
     }
 
-    private void getCountryDailyData() {
+    private void getCountryDailyData(String countryName) {
         Retrofit retrofit = RetrofitClientInstance.getRetrofitInstance(getActivity().getApplicationContext());
         CovidDataApi covidDataApi = retrofit.create(CovidDataApi.class);
-        Call<CountryDaily[]> call = covidDataApi.getDailyCountryData("2020-04-01", "Italy");
+        Call<CountryDaily[]> call = covidDataApi.getDailyCountryData("2020-04-01", countryName);
         call.enqueue(new Callback<CountryDaily[]>() {
             @Override
             public void onResponse(Call<CountryDaily[]> call, Response<CountryDaily[]> response) {
                 CountryDaily[] countryDaily = response.body();
                 List<DataEntry> data = new ArrayList<>();
                 Log.d(TAG, "onResponse: CountryDaily response" + response.body());
-                Log.d(TAG, "onResponse: CountryDaily response" + countryDaily.length);
                 CountryDaily.Province province = countryDaily[0].getProvinces().get(0);
                 data.add(new ValueDataEntry("confirmed", province.getConfirmed()));
                 data.add(new ValueDataEntry("recovered", province.getRecovered()));
                 data.add(new ValueDataEntry("deaths", province.getDeaths()));
                 data.add(new ValueDataEntry("active", province.getActive()));
+                Log.d(TAG, "onResponse:  CountryName returned by API" +countryDaily[0].getCountry() + "\n" +province.getConfirmed());
                 setCountryLineChart(data);
             }
 
@@ -130,6 +136,7 @@ public class CountryFragment extends Fragment {
             APIlib.getInstance().setActiveAnyChartView(countryPieView);
             pie = AnyChart.pie();
             pie.data(data);
+            Log.d(TAG, "setCountryLineChart: " + data.get(0).toString());
             countryPieView.setChart(pie);
         });
     }
@@ -139,9 +146,16 @@ public class CountryFragment extends Fragment {
             APIlib.getInstance().setActiveAnyChartView(countryLineView);
             line = AnyChart.pie();
             line.data(data);
+            Log.d(TAG, "setCountryLineChart: " + data.get(0).getValue("confirmed"));
             countryLineView.setChart(line);
         },2000);
     }
 
-
+    public void searchCountry(String countryName){
+        getCountryLatestData(countryName);
+        new Handler(Looper.getMainLooper()).postDelayed(()-> {
+                    getCountryDailyData(countryName);
+                },2000
+        );
+    }
 }
